47ffe118b10a94bda6dc5b80fc92e72b8c9cf8f9
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-06-12 17:12:40 +0200

    feat: add base
    Signed-off-by: Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,8 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'Albert Hernandez!',
+  base: '/hello/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
