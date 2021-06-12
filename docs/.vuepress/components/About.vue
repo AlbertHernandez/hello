2f90901853bdcee77d15344cd8563af4fc02ac5e
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-06-12 16:29:56 +0200

    feat: improve homepage
    Signed-off-by: Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>

--- docs/.vuepress/components/About.vue
+++ docs/.vuepress/components/About.vue
@@ -69,18 +69,16 @@
     </div>
 
     <div class="introduction">
+      <h2> Hi There, I'm Albert! <img src="https://raw.githubusercontent.com/ABSphreak/ABSphreak/master/gifs/Hi.gif" width="30px"></h2>
+
       <p>
-        I'm a
-        <span
-            class="typing"
-            data-period="2000"
-            style="color: #1DA1F2; font-weight: bold"
-            data-rotate='[ " Frontend ", " Backend ", " Fullstack " ]'>
-            </span>
-        Developer. I'm a passionate learner who loves the code.
+        I'm a Software Engineer 🚀 from Spain 🇪🇸, currently working 👨🏻‍💻 at <em><a href="https://www.coverwallet.com/">Coverwallet, an Aon Company</a></em>.
       </p>
       <p>
-        You'll always find me learning something new.
+        I'm a passionate learner who loves the code ☕︎. You'll always find me learning something new.
+      </p>
+      <p>
+        Beside's programming, I love Apple 🍎, watching Netflix 📺 and delicious food 🌯 🍱.
       </p>
     </div>
 
@@ -114,62 +112,5 @@
 <script>
 export default {
   name: "Introduction",
-};
-
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('typing');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typing > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
 };
 </script>
